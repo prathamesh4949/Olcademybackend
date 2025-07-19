8acb9c014cea49433bbad6c8e8404cbea9a80374
prathamesh4949 <prathmeshmahajan5835@gmail.com>
2025-07-19 15:43:18 +0530

    Update UserRoutes.js

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -1,4 +1,4 @@
-import express from 'express'
+/*import express from 'express'
 import { login, register, resendOtp, verifyEmail } from '../controllers/userController.js';
 
 const router = express.Router();
@@ -8,5 +8,18 @@ router.post('/login', login)
 router.post('/signup/verify-email', verifyEmail)
 router.post('/signup/resend-otp', resendOtp)
 
+
+export default router;
+*/
+// routes/userRoute.js
+import express from 'express'
+import { login, register, resendOtp, verifyEmail } from '../controllers/userController.js';
+
+const router = express.Router();
+
+router.post('/signup', register)
+router.post('/login', login)
+router.post('/signup/verify-email', verifyEmail)
+router.post('/signup/resend-otp', resendOtp)
 
 export default router;
